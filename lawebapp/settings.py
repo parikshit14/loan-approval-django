e91b4017c85668f347a7117f292e88883285175b
parikshit14 <14.parikshitsingh@gmail.com>
2020-07-16 18:46:42 +0530

    first commit

--- lawebapp/settings.py
+++ lawebapp/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = 'lawebapp/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'firstpage/static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'lawebapp/static/')
 #STATICFILES_DIR=(os.path.join(BASE_DIR,'firstpage/static'))
