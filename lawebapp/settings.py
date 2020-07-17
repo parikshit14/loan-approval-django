0b6de693ecb7d185fd2a70816f05f4d38b4ecb2c
parikshit14 <14.parikshitsingh@gmail.com>
2020-07-17 15:58:26 +0530

    first commit

--- lawebapp/settings.py
+++ lawebapp/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-#STATICFILES_DIR=(os.path.join(BASE_DIR,'firstpage/static'))
+STATICFILES_DIR=(os.path.join(BASE_DIR,'firstpage/static'))
